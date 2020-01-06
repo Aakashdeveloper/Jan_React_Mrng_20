48752a703db11883d0b88ab35b7368e75d7641d1
aakashDeveloper <ahanda205@gmail.com>
2020-01-06 01:18:31 +0000

    a

---
+++ myapp/src/component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <center>
+                <h2>React App</h2>
+            </center>
+            <hr/>
+        </div>
+    )
+}
+
+export default Header;
