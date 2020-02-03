492a7b36141be760a1b6f0435af191a5660f8dab
aakashDeveloper <ahanda205@gmail.com>
2020-02-03 01:22:41 +0000

    a

--- redux2/src/actions/index.js
+++ redux2/src/actions/index.js
@@ -29,3 +29,40 @@ export function galleryNewsA(){
         payload:output
     }
 }
+
+export function selectedNews(id){
+    const output = fetch(`${url}/articles?id=${id}`,{method:'GET'})
+    .then(response => response.json())
+
+    return{
+        type:'GET_SELECTED_NEWS',
+        payload:output
+    }
+}
+
+export function clearselectedNews(id){
+
+    return{
+        type:'CLEAR_SELECTED_NEWS',
+        payload:[]
+    }
+}
+
+
+export function handleLikes(array,id){
+    const output =fetch(`${url}/articles/${id}`,
+    {
+        method:'PATCH',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({likes:array})
+    })
+    .then(data => data.json()) 
+
+    return{
+        type:'HANDLE_LIKES',
+        payload:output
+    }
+}
--- redux2/src/container/NewsDetails.js
+++ redux2/src/container/NewsDetails.js
@@ -1,15 +1,68 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
-
+import {selectedNews,clearselectedNews} from '../actions';
+import LikeCounter from './LikeCounter';
 
 class NewsDetails extends Component {
+
+    componentDidMount(){
+        this.props.dispatch(selectedNews(this.props.match.params.id))
+    }
+
+    renderNews = ({selected}) => {
+        if(selected){
+            return selected.map((data) => {
+                return(
+                    <div key={data.id}>
+                        <div className="tags">
+                            <span>
+                                <i className="fa fa-eye">
+                                    {data.views}
+                                </i>
+                            </span>
+                            <span>
+                                <i className="fa fa-thumbs-up">
+                                    {data.likes[0]}
+                                </i>
+                            </span>
+                            <span>
+                                <i className="fa fa-thumbs-down">
+                                    {data.likes[1]}
+                                </i>
+                            </span>
+                        </div>
+                        <div className="top">
+                            <h2>{data.title}</h2>
+                            <span>
+                                Article By: {data.author}
+                            </span>
+                            <img src={`/images/articles/${data.img}`}/>
+                            <div className="body_news">
+                                {data.body}
+                            </div>
+                            <div>
+                               <LikeCounter
+                               articleId={data.id}
+                               likes={data.likes[0]}
+                               dislikes={data.likes[1]}/>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+        }
+    }
     render(){
         return(
-            <div>
-                <h1>News Details</h1>
+            <div className="news_container">
+                {this.renderNews(this.props.details)}
             </div>
         )
+    }
+
+    componentWillUnmount(){
+        this.props.dispatch(clearselectedNews())
     }
 }
 
